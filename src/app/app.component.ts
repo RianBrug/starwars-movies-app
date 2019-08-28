daed4a22d93337f47a4ddb153b30d646c82775b5
Rian Bruggemann <riancvb@gmail.com>
2019-08-28 05:06:51 -0300

    changed logic to go into the details section of the movie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,3 @@
-// import { Component } from '@angular/core';
-//
-// @Component({
-  // selector: 'app-root',
-  // templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.scss']
-// })
-// export class AppComponent {
-//   title = 'starwars-movies-page';
-// }
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,8 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-//
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { SidebarComponent } from './sidebar/sidebar.component';
-// import { PostsComponent } from './posts/posts.component';
-// import { DetailsComponent } from './details/details.component';
-//
-// import { HttpClientModule } from  '@angular/common/http';
-// import { DataService } from './data.service';
-//
-// @NgModule({
-//   declarations: [
-    // AppComponent,
-    // SidebarComponent,
-    // PostsComponent,
-    // DetailsComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule
-//   ],
-//   providers: [DataService],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-// used to create fake backend
 import { fakeBackendProvider } from './_helpers';
 
 import { appRoutingModule } from './app.routing';
